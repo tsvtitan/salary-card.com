ab0fa2df9c469c0089c74cf6bb38f270137f1644
Sergei <tsv.titan@gmail.com>
2015-05-13 22:58:12 +0300

    Update remount.sh

--- remount.sh
+++ remount.sh
@@ -4,10 +4,10 @@ echo "Unmounting..."
 
 SC_HOME=`dirname $0`
 
-umount -f $SC_HOME/www
+sudo umount -f $SC_HOME/www
 
 echo "Mounting..."
 
-echo "t6)uDUgy" | sshfs root@salary-card.com:/www $SC_HOME/www -p 50022 -o cache=no -o reconnect -o volname=salary-card.com -o password_stdin
+echo "t6)uDUgy" | sshfs root@salary-card.com:/www $SC_HOME/www -p 50022 -o cache=no -o reconnect -o password_stdin
 
 echo "Done."
